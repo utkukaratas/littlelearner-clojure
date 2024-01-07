2f2a591840febd684707b0c00981d191f2f7fe4d
utkukaratas <m.utku.k@gmail.com>
2024-01-07 21:04:11 +0300

    still at chapter 3

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] [io.github.nextjournal/clerk "0.15.957"]]
+  :dependencies [
+                 [org.clojure/clojure "1.11.1"]
+                 [io.github.nextjournal/clerk "0.15.957"]
+                 [org.clojure/tools.trace "0.7.11"]]
   :main ^:skip-aot littlelearner0.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/littlelearner0/matrixops.clj
+++ src/littlelearner0/matrixops.clj
@@ -1,8 +1,9 @@
 (ns littlelearner0.matrixops
   #_{:clj-kondo/ignore [:refer-all]}
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all])
+  (:require [clojure.tools.trace :refer :all]))
 
-;; extendend `+` and `*` operations defined in the book.
+;; extended `+` and `*` operations defined in the book.
 ;; yet the book doesn't provide any implementations.
 
 ;; xxx check for matrix type in clojure
@@ -15,23 +16,27 @@
     ;; both scalars
     (and (number? x) (number? y)) (+ x y)
 
-    ;; x is a scalar
+    ;; If the first is a number and second is a list, add the number to each element of the list
     (number? x) (map (fn [elem]
-                       (if (vector? elem)
-                         (tns+ x elem)
-                         (+ x elem)))
+                       (tns+ x elem))
                      y)
+    ;; If the first is a list and the second is a number, add the number to each element of the list
+    (number? y) (map (fn [elem]
+                       (tns+ elem x))
+                     x)
 
-    ;; x is a tensor, y is scalar
-    :else (map (fn [elem1 elem2]
-                 (if (vector? elem1)
-                   (tns+ elem1 elem2)
-                   (+ elem1 elem2)))
-               x y)))
+    ;; thanks chatgpt!
+    (and (coll? x) (every? coll? y))
+    (map (fn [nested-list] (tns+ x nested-list)) y)
+
+    ;; If both are lists, add corresponding elements
+    :else
+    (map tns+ x y)))
 
 (is (= [5 5] (tns+ [4 4] [1 1])))
 (is (= [6 6] (tns+ 4 [2 2])))
 (is (= [9] (tns+ [2] [7])))
+
 
 ;; In mathematics and tensor operations, adding two tensors of different
 ;; ranks is not typically defined. The rank of a tensor refers to the
@@ -39,8 +44,10 @@
 ;; tensor, a matrix is a rank-2 tensor, etc.) .
 ;; For tensors to be added together, they must have the same shape
 ;; and rank.
+;; chatgpt did this!
 (is (= [[10 12 9] [13 13 8]] (tns+ [6 9 1] [[4 3 8] [7 4 7]])))
 
+;; ok now what:
 (is (= [[5 8 9] [8 3 2]]
        (tns+ [[4 6 7] [2 0 1]] [[1 2 2] [6 3 1]])))
 
